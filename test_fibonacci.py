6d6a0062762325b03cf464673df80230f23ac0aa
Tarren Meyers <thetarrenmeyers@gmail.com>
2021-03-04 22:14:10 -0800

    copied from last inclass but included coverage tests

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -9,7 +9,7 @@ class TestCase(unittest.TestCase):
         self.assertEqual(fibonacci.fibo(14),377)
     def test_fact1(self):
         self.assertEqual(fibonacci.fact(6),720)
-    def test_fibo2(self):
+    def test_fact2(self):
         self.assertEqual(fibonacci.fact(8),40320)
 
 
